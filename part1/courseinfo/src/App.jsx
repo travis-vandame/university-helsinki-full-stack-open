f61bd635ba1580b1c3dc79b8f9e40202f4520a8c
travis-vandame <travis.vandame@gmail.com>
2026-07-24 22:32:02 -0500

    refactor(app): added new components Header, Content, Part and Total

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,47 @@
+const Header = ({ course }) => {
+  return(
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return(
+    <p>{name} {exercises}</p>    
+  )
+}
+
+const Content = ({ parts }) => {
+  return(
+    <>
+    {parts.map(({ name, exercises, id }) => (
+      <Part key={id} name={name} exercises={exercises} />
+    ))}
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return(
+    <p>Number of exercises { total }</p>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10, id: 0 },
+      { name: 'Using props to pass data', exercises: 7, id: 1 },
+      { name: 'State of a component', exercises: 14, id: 2 }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
