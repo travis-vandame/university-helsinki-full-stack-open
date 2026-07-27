8b0a635469032605fd4d1196017dda778c70d73a
travis-vandame <travis.vandame@gmail.com>
2026-07-27 17:30:55 -0500

    chore(app, main): Final code cleanup after part1 sections a, b and c

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -2,25 +2,3 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(<App />)
-/*
-let counter = 1
-
-const root = createRoot(document.getElementById('root'))
-
-const refresh = () => {
-  root.render(
-    <App counter={counter} />
-  )
-}
-
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
-*/
