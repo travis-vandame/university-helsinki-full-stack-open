fe718a7655896b52c48ff3e7f2dff4438a043a2c
travis-vandame <travis.vandame@gmail.com>
2026-07-29 20:54:43 -0500

    chore: Recreated Part1 courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = ({ course }) => {
+  return(
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return(
+    <>
+      <p>{name} {exercises}</p>    
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return(
+    <>
+    {parts.map(({ name, exercises, id }) => (
+      <Part key={id} name={name} exercises={exercises} />
+    ))}
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return(
+    <>
+      <p>Number of exercises { total }</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10, id: 0 },
+      { name: 'Using props to pass data', exercises: 7, id: 1 },
+      { name: 'State of a component', exercises: 14, id: 2 }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
